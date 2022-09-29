87c7a42096f72e0162bfb66cb076baed1e3476bb
Nokuthulax <wendynokuthula753@gmail.com>
2022-09-29 13:44:09 -0600

    assignment 1D

---
+++ assets/js/scripts.js
@@ -0,0 +1,19 @@
+/*!
+* Start Bootstrap - Bare v5.0.8 (https://startbootstrap.com/template/bare)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+var data = [];
+function loadData(){
+    data.forEach(student=>{
+        var name = window.prompt("enter your name");
+        var age = window.prompt("enter your age");
+        var averageMark = window.prompt("enter your averageMark");
+        var teacher = window.prompt("enter your teacher");
+        
+
+    });
+}
